ddee8fbedc0eed2845db2b001c22b20f4c0c9880
Anders Börjesson <anders.g.borjesson@volvocars.com>
2024-10-27 12:02:18 +0100

    some minor thing in main

--- src/main.go
+++ src/main.go
@@ -13,41 +13,6 @@ import (
 	"github.com/prometheus/procfs"
 )
 
-// func Test() {
-// 	nf, err := netflow.New(
-// 		netflow.WithCaptureTimeout(5 * time.Second),
-// 	)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = nf.Start()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer nf.Stop()
-
-// 	<-nf.Done()
-
-// 	var (
-// 		limit     = 50
-// 		recentSec = 5
-// 	)
-
-// 	rank, err := nf.GetProcessRank(limit, recentSec)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	bs, err := json.MarshalIndent(rank, "", "    ")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Println(string(bs))
-
-// }
-
 func Collect(parseFsFreq int64) {
 
 	defer fmt.Println("Defferd")
